4baface1ba377df9383866db7e081cdf745f65f4
Cameron Moir <cameronjmoir@gmail.com>
2019-09-20 13:37:22 +0100

    tested guest has a name

--- specs/guests_spec.rb
+++ specs/guests_spec.rb
@@ -4,8 +4,13 @@ require_relative("../guests")
 
 class GuestTest < MiniTest::Test
 
+  def setup
+    @guest = Guest.new("Cameron", 50)
+  end
 
-
+  def test_guest_has_name
+    assert_equal("Cameron", @guest.guest_name)
+  end
 
 
 
