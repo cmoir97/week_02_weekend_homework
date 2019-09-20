93fcc6353081fde2bf2e704e9dd1155bd01bf91e
Cameron Moir <cameronjmoir@gmail.com>
2019-09-20 13:40:04 +0100

    tested song has a name

--- songs.rb
+++ songs.rb
@@ -1,5 +1,5 @@
 class Song
-
+  attr_reader :song_name
 def initialize(song_name)
   @song_name = song_name
 end
--- specs/songs_spec.rb
+++ specs/songs_spec.rb
@@ -4,10 +4,13 @@ require_relative("../songs")
 
 class SongTest < MiniTest::Test
 
+  def setup
+    @song = Song.new("Small Poppies")
+  end
 
-
-
-
+  def test_song_has_name
+    assert_equal("Small Poppies", @song.song_name)
+  end
 
 
 
